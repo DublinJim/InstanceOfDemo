63bc75d1c9073a9418a3f79f76b895bdc126f249
James Keogh <jkeogh@adga.ca>
2022-08-19 11:25:48 -0400

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+
+Student bob = new Student(32,"Bob");
+
+
+bob.sayWho();
+
+        System.out.println(bob instanceof Student);
+    }
+}
---
+++ src/main/java/Student.java
@@ -0,0 +1,22 @@
+public class Student {
+
+    private int id;
+    private String name;
+
+    public Student(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void sayWho() {
+        System.out.println("Name :"+name+" id="+id);
+    }
+}
